d02d70eca6517f17caee508b3ab7095d192e9676
Colin Campbell-McPherson <colin@hhd.com.au>
2010-09-08 18:12:49 +1000

    removed unnessisary files

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,2 @@
 class OrdersController < ApplicationController
-  before_filter :create_or_find_order
-
-  def billing
-    @order.billing_address ||= Address.new
-  end
-
- protected
-
-  def create_or_find_order
-    if session[:order_id].nil? 
-      @order = Order.create
-      session[:order_id] = @order.id
-    else
-      @order = Order.find(session[:order_id])
-    end
-  end
-
 end
