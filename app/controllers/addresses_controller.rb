d600e643718afc1ddf0b97397ffd781280db7365
Colin Campbell-McPherson <colin@hhd.com.au>
2010-09-08 15:03:11 +1000

    moved address form out to a partial implemented address creation

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,10 +1,19 @@
 class AddressesController < ApplicationController
 
   def create
+    order = Order.find(session[:order_id])
+    order.build_shipping_address(params[:address])
+
+    if order.save
+      redirect_to billing_order_path
+    else
+      render :template => "orders/shipping"
+    end
   end
 
   def update
     address = Address.find(params[:id])
+
     if address.update_attributes(params[:address])
       redirect_to billing_order_path
     else
