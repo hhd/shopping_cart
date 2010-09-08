b76ef3d101bb7302cb9ea8f9d9edecec90908556
Colin Campbell-McPherson <colin@hhd.com.au>
2010-09-08 14:36:34 +1000

    Revert "switched around the order/address relationship"
    This reverts commit b590ced194249f65711da2befe37f3380472fe35.
    
    Conflicts:
    
    	app/models/address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,3 @@
 class Address < ActiveRecord::Base
-  belongs_to :order
   validates_presence_of :name, :line_one, :suburb, :post_code, :country
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   has_many :purchases
-  has_one :billing_address,  :class_name => "Address"
-  has_one :shipping_address, :class_name => "Address"
+  belongs_to :billing_address,  :class_name => "Address", :foreign_key => :billing_address_id
+  belongs_to :shipping_address, :class_name => "Address", :foreign_key => :shipping_address_id
 
   def total
     self.purchases.reduce(0){|t, p| t + p.sub_total }
